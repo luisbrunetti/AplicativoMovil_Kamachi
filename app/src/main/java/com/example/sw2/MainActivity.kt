505b3427a4cb2346fbc6084b5126449af3f172be
LuisBrunetti <luisbruno777@gmail.com>
2020-03-20 02:43:59 -0500

    Commit antes de la acutalización Android X

--- app/src/main/java/com/example/sw2/MainActivity.kt
+++ app/src/main/java/com/example/sw2/MainActivity.kt
@@ -2,33 +2,31 @@ package com.example.sw2
 
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.sw2.framents.AfiliacionFragment
+import com.example.sw2.framents.ContratosFragment
 import com.example.sw2.framents.HomeFragment
 import com.example.sw2.framents.SearchFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var selectedFragment : Fragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNav.setOnNavigationItemSelectedListener(navListener)
-
     }
-
     private val navListener: BottomNavigationView.OnNavigationItemSelectedListener =
-        BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            var selectedFragment: Fragment
+        BottomNavigationView.OnNavigationItemSelectedListener {item ->
             when (item.itemId) {
                 R.id.nav_home -> selectedFragment = HomeFragment()
                 R.id.nav_afiliación -> selectedFragment = AfiliacionFragment()
                 R.id.nav_Busqueda -> selectedFragment = SearchFragment()
+                R.id.nav_contratos -> selectedFragment = ContratosFragment()
             }
             supportFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
             true
