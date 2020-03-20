c043ba4abbc1dc8e3c934e888d35c1a4b0b93dc2
LuisBrunetti <luisbruno777@gmail.com>
2020-03-20 01:20:08 -0500

    Commit antes de la acutalización Android X

--- app/src/main/java/com/example/sw2/LoginActivity.kt
+++ app/src/main/java/com/example/sw2/LoginActivity.kt
@@ -30,7 +30,13 @@ class LoginActivity : AppCompatActivity() {
         txtPassword = findViewById(R.id.editPassword)
         pogressBarLogin = findViewById(R.id.progressBar2)
 
-
+        window.decorView.apply {
+            // Hide both the navigation bar and the status bar.
+            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+            // a general rule, you should design your app to hide the status bar whenever you
+            // hide the navigation bar.
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
 
     }
 
--- app/src/main/java/com/example/sw2/MainActivity.kt
+++ app/src/main/java/com/example/sw2/MainActivity.kt
@@ -1,14 +1,36 @@
 package com.example.sw2
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.sw2.framents.AfiliacionFragment
+import com.example.sw2.framents.HomeFragment
+import com.example.sw2.framents.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.setOnNavigationItemSelectedListener(navListener)
+
     }
 
-
+    private val navListener: BottomNavigationView.OnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            var selectedFragment: Fragment
+            when (item.itemId) {
+                R.id.nav_home -> selectedFragment = HomeFragment()
+                R.id.nav_afiliación -> selectedFragment = AfiliacionFragment()
+                R.id.nav_Busqueda -> selectedFragment = SearchFragment()
+            }
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
+            true
+        }
 }
