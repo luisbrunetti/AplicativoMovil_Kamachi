bb713ade49f4e6c6458baee451042da1222eb513
luisbrunetti <57022894+luisbrunetti@users.noreply.github.com>
2020-03-22 07:49:21 +0100

    Commit antesd e intentar realizar el listVIew

--- app/src/main/java/com/example/sw2/Clases/ServicioListView.kt
+++ app/src/main/java/com/example/sw2/Clases/ServicioListView.kt
@@ -1,13 +1,12 @@
 package com.example.sw2.Clases
 
-class ServicioListView(NombreTrabajo:String ,Distrito:String,Imagen:Int){
-    var NombreTrabaj:String = ""
-    var Distrito:String = ""
-    var Imagen:Int =  0
+class ServicioListView(NombreTrabajo:String,Distrito:String,Imagen:String){
+    /*var NombreTrabaj:String? = ""
+    var Distrito:String? = ""
+    var Imagen:String? =  ""
     init{
         this.NombreTrabaj = NombreTrabajo
         this.Distrito = Distrito
         this.Imagen = Imagen
-    }
-
+    }*/
 }
